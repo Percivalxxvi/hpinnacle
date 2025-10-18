6140d2088eb0fed095af004546fe2f92dae0a0f7
Percivalxxvi <ayodejioladeji12@gmail.com>
2025-10-18 19:06:01 +0100

    version 3.3.1

--- components/Homeslide.jsx
+++ components/Homeslide.jsx
@@ -22,13 +22,13 @@ const Homeslide = () => {
     pauseOnHover: false
   };
   return (
-    <div className='h-[30vh] md:h-[80vh]'>
+    <div className='h-[30vh] md:h-[80vh] lg:h-[30vh] 2xl:[45vh]'>
         <div className="h-[30vh] md:h-[70%]">
       <Slider {...settings}>
         <div>
-            <div style={{background:`url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT1hsT6DLYHtSRxBysrLfnUP1cM1eqsrbWzQ&s")`, backgroundRepeat:'no-repeat', backgroundSize:'cover', backgroundPosition:'center center'}} className="flex items-center justify-center h-[30vh] bg-red-600 w-[100%] md:h-[80vh]">
+            <div style={{background:`url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT1hsT6DLYHtSRxBysrLfnUP1cM1eqsrbWzQ&s")`, backgroundRepeat:'no-repeat', backgroundSize:'cover', backgroundPosition:'center center'}} className="flex items-center justify-center h-[30vh] bg-red-600 w-[100%] md:h-[80vh] lg:h-[30vh] 2xl:[45vh]">
                 <div style={{backgroundColor:('rgba(23, 0, 0, 0.5)'),justifyItems:'center', alignContent:'center' }} className='h-[100%] w-[100%]'>
-                    <h1 className='text-4xl md:text-8xl md:w-[60%] font-bold text-white text-center hover:scale-103 cursor-pointer'> <TypewriterLoop words={['HEROES PINNACLE SCHOOLS']}
+                    <h1 className='text-4xl md:text-8xl lg:text-6xl md:w-[60%] font-bold text-white text-center hover:scale-103 cursor-pointer'> <TypewriterLoop words={['HEROES PINNACLE SCHOOLS']}
         typingSpeed={100}
         deletingSpeed={20}
         delay={2000}/></h1>
@@ -36,16 +36,16 @@ const Homeslide = () => {
             </div>
         </div>
         <div>
-            <div style={{background:`url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvPjY2BmRH4G1Tu2kPScbhExHPNoVw6tO8FQ&s")`, backgroundRepeat:'no-repeat', backgroundSize:'cover', backgroundPosition:'center center'}} className="flex items-center justify-center h-[30vh] bg-blue-600 w-[100%] md:h-[80vh]">
+            <div style={{background:`url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvPjY2BmRH4G1Tu2kPScbhExHPNoVw6tO8FQ&s")`, backgroundRepeat:'no-repeat', backgroundSize:'cover', backgroundPosition:'center center'}} className="flex items-center justify-center h-[30vh] bg-blue-600 w-[100%] md:h-[80vh] lg:h-[30vh] 2xl:[45vh]">
             <div style={{backgroundColor:('rgba(23, 0, 0, 0.5)'),justifyItems:'center', alignContent:'center' }} className='h-[100%] w-[100%]'>
-                    <h1 className='text-4xl md:text-8xl md:w-[60%] font-bold text-white text-center hover:scale-103 cursor-pointer'>HEROES PINNACLE SCHOOLS</h1>
+                    <h1 className='text-4xl md:text-8xl lg:text-6xl md:w-[60%] font-bold text-white text-center hover:scale-103 cursor-pointer'>HEROES PINNACLE SCHOOLS</h1>
                 </div>
             </div>
         </div>
         <div>
-            <div style={{background:`url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrw_6IbimmhEjmSp1X3vOJfLFOo1fJhuOB3w&s")`, backgroundRepeat:'no-repeat', backgroundSize:'cover', backgroundPosition:'center center'}} className="flex items-center justify-center h-[30vh] bg-yellow-300 w-[100%] md:h-[80vh]">
+            <div style={{background:`url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrw_6IbimmhEjmSp1X3vOJfLFOo1fJhuOB3w&s")`, backgroundRepeat:'no-repeat', backgroundSize:'cover', backgroundPosition:'center center'}} className="flex items-center justify-center h-[30vh] bg-yellow-300 w-[100%] md:h-[80vh] lg:h-[30vh] 2xl:[45vh]">
             <div style={{backgroundColor:('rgba(23, 0, 0, 0.5)'),justifyItems:'center', alignContent:'center' }} className='h-[100%] w-[100%]'>
-                    <h1 className='text-4xl md:text-8xl md:w-[60%] font-bold text-white text-center hover:scale-103 cursor-pointer'><TypewriterLoop words={['HEROES PINNACLE SCHOOLS']}
+                    <h1 className='text-4xl md:text-8xl lg:text-6xl md:w-[60%] font-bold text-white text-center hover:scale-103 cursor-pointer'><TypewriterLoop words={['HEROES PINNACLE SCHOOLS']}
         typingSpeed={100}
         deletingSpeed={20}
         delay={5000}/></h1>
@@ -53,9 +53,9 @@ const Homeslide = () => {
             </div>
         </div>
         <div>
-            <div style={{background:`url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvPjY2BmRH4G1Tu2kPScbhExHPNoVw6tO8FQ&s")`, backgroundRepeat:'no-repeat', backgroundSize:'cover', backgroundPosition:'center center'}} className="flex items-center justify-center h-[30vh] bg-blue-600 w-[100%] md:h-[80vh]">
+            <div style={{background:`url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvPjY2BmRH4G1Tu2kPScbhExHPNoVw6tO8FQ&s")`, backgroundRepeat:'no-repeat', backgroundSize:'cover', backgroundPosition:'center center'}} className="flex items-center justify-center h-[30vh] bg-blue-600 w-[100%] md:h-[80vh] lg:h-[30vh] 2xl:[45vh]">
             <div style={{backgroundColor:('rgba(23, 0, 0, 0.5)'),justifyItems:'center', alignContent:'center' }} className='h-[100%] w-[100%]'>
-                    <h1 className='text-4xl md:text-8xl md:w-[60%] font-bold text-white text-center hover:scale-103 cursor-pointer'>HEROES PINNACLE SCHOOLS</h1>
+                    <h1 className='text-4xl md:text-8xl lg:text-6xl md:w-[60%] font-bold text-white text-center hover:scale-103 cursor-pointer'>HEROES PINNACLE SCHOOLS</h1>
                 </div>
             </div>
         </div>
