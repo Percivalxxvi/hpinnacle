efad1558ee9c237e75dcdb0d1df12cdd63099177
Percivalxxvi <ayodejioladeji12@gmail.com>
2025-10-21 13:51:35 +0100

    version 3.5

--- pages/About.jsx
+++ pages/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
       </div>
       <Navpc page="about"/>
       <div className='flex flex-col items-center justify-center lg:h-fit'>
-          <div className='h-fit w-full flex flex-col items-center justify-center lg:h-100 lg:flex-row lg:mt-20 mt-10 border-b-4 border-[gold]'>
+          <div className='h-fit w-full flex flex-col items-center justify-center lg:h-100 lg:flex-row lg:mt-20 mt-10 border-b-2 border-[gold]'>
               <div className='lg:h-100 lg:w-200'>
                 <img className='lg:h-4/4 object-cover lg:w-200' src={school2} alt="" />
               </div>
@@ -27,8 +27,8 @@ const About = () => {
             
           </div>
           <div className='h-fit w-full flex flex-col items-center justify-center lg:h-100 lg:flex-row-reverse lg:mt-20 mt-10 border-b-4 border-[gold] lg:justify-between'>
-              <div className='lg:h-100 lg:w-150 h-70 w-full bg-amber-400 border-2 border-[gold]'>
-                <img className='lg:h-4/4 lg:object-cover lg:w-150 h-70 w-full object-cover' src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBvcnRyYWl0fGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=600" alt="" />
+              <div className='lg:h-100 lg:w-150 h-70 w-full bg-amber-400'>
+                <img className='lg:h-4/4 lg:object-cover lg:w-150 h-70 w-full object-cover border-2 border-[gold]' src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBvcnRyYWl0fGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=600" alt="" />
               </div>
               <div className='lg:ml-10 lg:h-100 lg:w-3/4 flex flex-col justify-evenly p-5 h-85 w-full items-center'>
                 <h1 className='text-4xl lg:text-6xl lg:text-left lg:w-[95%] w-[95%] text-center'>About the Proprietress</h1>
@@ -37,9 +37,9 @@ const About = () => {
               </p>
               </div>
           </div>
-          <div className='h-fit w-full flex flex-col items-center justify-center lg:h-100 lg:flex-row lg:mt-20 mt-10 border-b-4 border-[gold] lg:justify-between'>
-              <div className='lg:h-100 lg:w-150 h-70 w-full bg-amber-400 border-2 border-[gold]'>
-                <img className='lg:h-4/4 lg:object-cover lg:w-150 h-70 w-full object-cover' src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTg2fHxwb3J0cmFpdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=600" alt="" />
+          <div className='h-fit w-full flex flex-col items-center justify-center lg:h-100 lg:flex-row lg:mt-20 mt-10 lg:border-b-2 border-[gold] lg:justify-between'>
+              <div className='lg:h-100 lg:w-150 h-70 w-full bg-amber-400'>
+                <img className='lg:h-4/4 lg:object-cover lg:w-150 h-70 w-full object-cover border-2 border-[gold]' src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTg2fHxwb3J0cmFpdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=600" alt="" />
               </div>
               <div className='lg:ml-10 lg:h-100 lg:w-3/4 flex flex-col justify-evenly p-5 h-85 w-full items-center'>
                 <h1 className='text-4xl lg:text-6xl lg:text-left lg:w-[95%] w-[95%] text-center'>About the Proprietor</h1>
